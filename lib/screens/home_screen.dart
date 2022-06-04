0245f341c75925328565c21985d8b50bda9cd794
Yusuf Boran <ybsrfn@gmail.com>
2022-06-04 19:38:29 +0300

    update

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -56,6 +56,7 @@ class _HomePageState extends State<HomePage> {
       ),
       body: ListView(
         children: [
+          SizedBox(height: 10),
           lastDayWidget(widget.lastDayValue),
           LineChartPage(
               items: widget.milkQuantity,
